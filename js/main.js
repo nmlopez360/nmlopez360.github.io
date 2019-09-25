7454ed83c9580105647798899df2e6b2da4a45e0
Nicole Lopez <nmlopez360@gmail.com>
2019-09-25 00:51:45 -0400

    Almost Finished
    Updated works section, fixed last resume spelling errors, completed footer, and added all links.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Smooth Scrolling Effect
 // Code from https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
 $("a[href^='#']").click(function(e) {
 	e.preventDefault();
@@ -7,4 +8,36 @@ $("a[href^='#']").click(function(e) {
 	$("body, html").animate({
 		scrollTop: position
 	} /* speed */ );
+});
+
+
+// Element Fade-In on Scroll
+// Code from https://codepen.io/annalarson/pen/GesqK
+
+$(function(){  // $(document).ready shorthand
+  $('.fullPage').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
 });
